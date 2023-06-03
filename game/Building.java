e8d52df9a81e36a4a63c0ebaa40304987f9a762a
frozen-coder <frozen.0246@gmail.com>
2023-06-03 11:29:45 -0700

    Building class

--- game/Building.java
+++ game/Building.java
@@ -1,7 +1,16 @@
 package game;
 
 public class Building {
-	public Building() {
+	//tile list
+	//set tile to somthing
+	private ArrayList<Tiles> tilesOccupied;
+	private int price;
+	private int cashFlow;
+	private String name;
+	private int pollution;
+	private int powerflow;
+
+	public Building(ArrayList<Tiles> tilesOccupiedIn, int priceIn, int cashFlowIn, String nameIn,  ) {
 		
 	}
 }
