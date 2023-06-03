f9307506ee156549f13270f41d48d72da1faa363
TienWin <tiendabest007@gmail.com>
2023-06-03 12:16:10 -0700

    powerplant constructior

--- game/PowerPlant.java
+++ game/PowerPlant.java
@@ -1,9 +1,11 @@
 package game;
-
+import java.util.*;
 public class PowerPlant extends Building {
 	private int generate;
 	
-	public PowerPlant(int x, int y, int length, int width) {
+	// maybe put in x, y later
+	public PowerPlant(ArrayList<Tile> occupied, int cost, int cashflow, String type, int pollution, int powerflow) {
 		// Use building class and put in the coordinates. 
+		super(occupied, cost, cashflow, type, pollution, powerflow);
 	}
 }
