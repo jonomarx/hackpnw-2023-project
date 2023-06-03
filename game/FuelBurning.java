a08c98ea20bc6590ba6ba244e4e7b3e073453ba8
TienWin <tiendabest007@gmail.com>
2023-06-03 14:21:16 -0700

    Update FuelBurning.java

---
+++ game/FuelBurning.java
@@ -0,0 +1,28 @@
+package game;
+import java.util.*;
+public class FuelBurning extends PowerPlant {
+	private static final int WIDTH;
+	private static final int LENGTH;
+	private static final int pollution;
+	private static final int flow;
+	private int cost;
+	private int x;
+	private int y;
+	
+	//coal constructor
+	public FuelBurning (ArrayList<Tile> t, int posx, int posy, int cost, int pollution, int flow, String type) {
+		super(t, cost, flow, )
+		x = posx;
+		y = posy;
+		this.cost = cost;
+		// Make a tile for the potential powerplant
+		Tile tile = new Tile(posx, posy);
+		
+		
+		
+		
+		
+		
+		
+	}
+}
