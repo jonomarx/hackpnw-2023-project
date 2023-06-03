0cc52722c836098dac5b30083334aa3fd1277912
jonomarx <jon.marx@me.com>
2023-06-03 10:32:44 -0700

    stuf

--- core/Main.java
+++ core/Main.java
@@ -1,9 +1,15 @@
 package core;
 
+import javax.swing.JFrame;
+
 public class Main {
+	private static final int WIDTH = 100;
+	private static final int HEIGHT = 100;
 
 	public static void main(String[] args) {
 		System.out.println("HI");
+		JFrame frame = new JFrame();
+		frame.setSize(WIDTH, HEIGHT);
 	}
 
 }
---
+++ core/Renderer.java
@@ -0,0 +1,12 @@
+package core;
+
+import java.awt.Graphics;
+
+public class Renderer {
+	// manage something involving stuff to render and the order of it
+	
+	
+	public static void render(Graphics g) {
+		// todo...
+	}
+}
