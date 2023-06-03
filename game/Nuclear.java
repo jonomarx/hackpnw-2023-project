0c39dfb3f8ebaa2d036ba4a7429ab0e8b19c7c91
TienWin <tiendabest007@gmail.com>
2023-06-03 15:15:13 -0700

    Update Nuclear.java

--- game/Nuclear.java
+++ game/Nuclear.java
@@ -1,5 +1,19 @@
 package game;
-
-public class Nuclear {
-
+import java.util.*;
+public class Nuclear extends PowerPlant{
+	private final int WIDTH = 1;
+	private final int LENGTH = 1;
+	private final int pollution = 0;
+	private final int cost = 100;
+	private final int x;
+	private final int y;
+	private final int cashflow = -80;
+	//coal constructor
+	public Nuclear (int posx, int posy, int cost, int pollution) {
+		super(posx, posy, 1, 1, 100, -50, "Nuclear", 20, 100);
+		// Check to see if the tile is aviliable.
+		x=posx;
+		y=posy;
+		
+	}	
 }
