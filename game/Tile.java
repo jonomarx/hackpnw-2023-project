c21c6921d39a7682085732cc8291d0a60a397fee
jonomarx <jon.marx@me.com>
2023-06-03 10:55:12 -0700

    Update Tile.java

--- game/Tile.java
+++ game/Tile.java
@@ -7,7 +7,7 @@ import core.Renderable;
 public class Tile implements Renderable {
 	private int x;
 	private int y;
-	private int content;
+	private int content; // an ID of whats on it, eg: 0 is 
 	public Tile(int x, int y) {
 		this.x = x;
 		this.y = y;
@@ -21,7 +21,13 @@ public class Tile implements Renderable {
 		return y;
 	}
 	
-	public void setOccu
+	public void setContent(int id) {
+		this.content = id;
+	}
+	
+	public int getContent(int id) {
+		return content;
+	}
 
 	@Override
 	public void render(Graphics g, int xoffset, int yoffset) {
