7f965d8861bc0f35c1025ac54624be1f63100d26
BuffTaco <jiayue.quan@gmail.com>
2023-06-03 17:43:40 -0700

    Update Solar.java

--- game/Solar.java
+++ game/Solar.java
@@ -1,16 +1,16 @@
 package game;
 import java.util.*;
 public class Solar extends PowerPlant{
-	private final int WIDTH = 1;
-	private final int LENGTH = 1;
-	private final int pollution = 0;
-	private final int cost = 50;
-	private final int x;
-	private final int y;
-	private final int cashflow = -3;
+	private static int WIDTH = 1;
+	private static int LENGTH = 1;
+	private static int pollution = 0;
+	private static int cost = 50;
+	private static int x;
+	private static int y;
+	private static int cashflow = -3;
 	//coal constructor
-	public Solar (int posx, int posy, int cost, int pollution) {
-		super(posx, posy, 1, 1, 50, 0, "Solar", 0, 100);
+	public Solar (int posx, int posy) {
+		super(posx, posy, WIDTH, LENGTH, cost, cashflow, "Solar", pollution, 100);
 		// Check to see if the tile is aviliable.
 		x=posx;
 		y=posy;
