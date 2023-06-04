6466e960c711db09720e4f85f306896437aa7809
BuffTaco <jiayue.quan@gmail.com>
2023-06-03 18:05:31 -0700

    Update Road.java

--- game/Road.java
+++ game/Road.java
@@ -3,10 +3,12 @@ package game;
 public class Road {
     private static int x;
     private static int y;
+    private static boolean horizontal;
 
-    public Road(int xIn, int yIn)
+    public Road(int xIn, int yIn, boolean horizontalIn)
     {
         x = xIn;
         y = yIn;
+        horizontal = horizontalIn;
     }
 }
