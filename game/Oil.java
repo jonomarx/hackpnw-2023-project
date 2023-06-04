242958dda24580b3993ad6f9342b19df7727e14a
BuffTaco <jiayue.quan@gmail.com>
2023-06-03 17:34:57 -0700

    final to static

--- game/Oil.java
+++ game/Oil.java
@@ -1,17 +1,17 @@
 package game;
 import java.util.*;
 public class Oil extends PowerPlant {
-	private final int WIDTH = 1;
-	private final int LENGTH = 1;
-	private final int pollution = 15;
-	private final int cost = 60;
-	private final int x;
-	private final int y;
-	private final int cashflow = -20;
+	private static int WIDTH = 1;
+	private static int LENGTH = 1;
+	private static int pollution = 15;
+	private static int cost = 60;
+	private static int x;
+	private static int y;
+	private static int cashflow = -20;
 	
 	//coal constructor
 	public Oil (int posx, int posy, int cost, int pollution) {
-		super(posx, posy, 1, 1, 60, -20, "Oil", 15, 75);
+		super(posx, posy, WIDTH, LENGTH, cost, cashflow, "Oil", pollution, 75);
 		// Check to see if the tile is aviliable.
 		x=posx;
 		y=posy;
--- game/Residental.java
+++ game/Residental.java
@@ -1,5 +1,17 @@
 package game;
 
-public class Residental {
+public class Residental extends Consumer {
+    private static int WIDTH = 4;
+    private static int HEIGHT = 4;
+    private static int price;
+    private static int cashFlow;
+    private static String name = "Residental";
+    private static int pollution;
+    private static int powerFlow;
 
+ public Residental(int xIn, int yIn)
+ {
+    super(xIn, yIn, WIDTH, HEIGHT, price, cashFlow, name, pollution, powerFlow);
+
+ }
 }
